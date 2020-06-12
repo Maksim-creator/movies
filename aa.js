db2310589e69c13dd575d068f48e8670f03d7661
maksim <maksimka.bocharov11@gmail.com>
2020-06-12 11:48:51 +0300

    first commit

---
+++ aa.js
@@ -0,0 +1,19 @@
+let numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+
+personalMovieDB = {
+count: numberOfFilms,
+movies: {},
+actors: {},
+geners: [],
+privat: false
+};
+
+let answer1 = prompt('Один из просмотренных фильмов?', '');
+let answer2 = prompt('На сколько оцените его?', '');
+let answer3 = prompt('Один из просмотренных фильмов?', '');
+let answer4 = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[answer1] = answer2;
+personalMovieDB.movies[answer3] = answer4;
+    
+console.log(personalMovieDB);
