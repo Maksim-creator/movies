53e66f0aa2f0b5cd4f654ab8ed16cfee52311f85
maksim <maksimka.bocharov11@gmail.com>
2020-06-16 15:11:26 +0300

    second commit

--- aa.js
+++ aa.js
@@ -8,12 +8,29 @@ geners: [],
 privat: false
 };
 
-let answer1 = prompt('Один из просмотренных фильмов?', '');
-let answer2 = prompt('На сколько оцените его?', '');
-let answer3 = prompt('Один из просмотренных фильмов?', '');
-let answer4 = prompt('На сколько оцените его?', '');
-
-personalMovieDB.movies[answer1] = answer2;
-personalMovieDB.movies[answer3] = answer4;
+for(let i = 0; i < 2; i++){
+    answer1 = prompt('Один из просмотренных фильмов?', '');
+    answer2 = prompt('На сколько оцените его?', '');
+   
+    if(answer1 != null && answer2 != null && answer1 != '' && answer2 != '' && answer1.length < 50){
+    personalMovieDB.movies[answer1] = answer2;
+    }else{
+        console.log('error');
+        i--;
+    }
+}
     
+
+switch(numberOfFilms){
+    case (personalMovieDB.count <= 10): 
+            alert('Просмотрено мало фильмов!');
+            break;
+    case (personalMovieDB.count > 10 && personalMovieDB.count <= 30 ):
+            alert('Вы класический зритель');
+            break;
+    case (personalMovieDB.count > 30):
+        alert('Вы киноман!');
+        break;
+   default: alert('Error');
+    }
 console.log(personalMovieDB);
